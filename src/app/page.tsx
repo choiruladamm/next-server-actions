c224e203d580bee596a8ca9d9e604d1e159528a2
choiruladamm <choiruladamm@gmail.com>
2023-10-25 20:45:24 +0700

    feat: adding an async/await function without caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,24 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
 
-export default function Home() {
+export interface Product {
+  id?: number;
+  product: string;
+  price: string;
+}
+
+export default async function Home() {
+  const res = await fetch(
+    "https://653918f7e3b530c8d9e7e7b1.mockapi.io/products",
+    {
+      cache: "no-cache",
+    }
+  );
+
+  const products: Product[] = await res.json();
+
   return (
-    <main className="mt-6">
+    <main className="my-6 max-w-4xl mx-auto">
       <h1 className="text-center text-3xl font-bold">Products Warehouse</h1>
 
       <form action="" className="max-w-lg flex flex-col gap-5 mx-auto p-5">
@@ -13,6 +28,16 @@ export default function Home() {
         <Button>Add Product</Button>
       </form>
 
+      <h2 className="font-bold p-5">List of Products</h2>
+      <div className="flex flex-wrap gap-5">
+        {products.map((product) => (
+          <div key={product.id} className="p-5 shadow">
+            <p className="font-semibold">{product.product}</p>
+            <p className="text-gray-600">$ {product.price}</p>
+          </div>
+        ))}
+      </div>
+      
     </main>
   );
 }
