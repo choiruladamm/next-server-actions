9223e4a19c895c011ca7826313e91ff4bc9c7bd0
choiruladamm <choiruladamm@gmail.com>
2023-10-25 20:31:03 +0700

    feat: add product warehouse example input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen place-items-center">
-      <Button>Next Server Actions</Button>
-    </div>
+    <main className="mt-6">
+      <h1 className="text-center text-3xl font-bold">Products Warehouse</h1>
+
+      <form action="" className="max-w-lg flex flex-col gap-5 mx-auto p-5">
+        <Input type="text" placeholder="Enter product name..." />
+        <Input type="text" placeholder="Enter price name..." />
+        <Button>Add Product</Button>
+      </form>
+
+    </main>
   );
 }
