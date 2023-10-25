cde93f71a92bf0b36e111c780faf6c0c5a91f98f
choiruladamm <choiruladamm@gmail.com>
2023-10-25 21:04:15 +0700

    feat: implementing a server actions in a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { FormEvent } from "react";
 
 export interface Product {
   id?: number;
@@ -9,22 +9,46 @@ export interface Product {
 }
 
 export default async function Home() {
-  const res = await fetch(
-    "https://653918f7e3b530c8d9e7e7b1.mockapi.io/products",
-    {
-      cache: "no-cache",
-    }
-  );
+  const urlMockAPi = "https://653918f7e3b530c8d9e7e7b1.mockapi.io/products";
+
+  const res = await fetch(urlMockAPi, {
+    cache: "no-cache",
+  });
 
   const products: Product[] = await res.json();
+
+  const addProductToDatabase = async (e: FormData) => {
+    'use server'
+    
+    const product = e.get("product")?.toString();
+    const price = e.get("price")?.toString();
+
+    if (!product || !price) return;
+
+    const newProduct: Product = {
+      product,
+      price,
+    };
+
+    await fetch(urlMockAPi, {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
   return (
     <main className="my-6 max-w-4xl mx-auto">
       <h1 className="text-center text-3xl font-bold">Products Warehouse</h1>
 
-      <form action="" className="max-w-lg flex flex-col gap-5 mx-auto p-5">
-        <Input type="text" placeholder="Enter product name..." />
-        <Input type="text" placeholder="Enter price name..." />
+      <form
+        action={addProductToDatabase}
+        className="max-w-lg flex flex-col gap-5 mx-auto p-5"
+      >
+        <Input name="product" placeholder="Enter product name..." />
+        <Input name="price" placeholder="Enter price name..." />
         <Button>Add Product</Button>
       </form>
 
@@ -37,7 +61,6 @@ export default async function Home() {
           </div>
         ))}
       </div>
-      
     </main>
   );
 }
