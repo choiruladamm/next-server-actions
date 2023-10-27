df8de5e4aa2251c0eea06f154bbfdd26130a524f
choiruladamm <choiruladamm@gmail.com>
2023-10-27 13:29:46 +0700

    chore: move folder & created example 2

---
+++ src/app/example-1/page.tsx
@@ -0,0 +1,48 @@
+import { addProductToDatabase } from "@/actions/serverAction";
+import AddProductButton from "@/components/AddProductButton";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Product } from "@/types/product";
+import * as React from "react";
+
+
+export default async function ExampleOne() {
+  const res = await fetch(
+    "https://653918f7e3b530c8d9e7e7b1.mockapi.io/products",
+    {
+      cache: "no-cache",
+      next: {
+        tags: ["produts"],
+      },
+    }
+  );
+
+  const products: Product[] = await res.json();
+
+  return (
+    <main className="my-6 max-w-4xl mx-auto">
+      <h1 className="text-center text-3xl font-bold">Products Warehouse</h1>
+
+      <AddProductButton>Add Macbook Pro</AddProductButton>
+
+      <form
+        action={addProductToDatabase}
+        className="max-w-lg flex flex-col gap-5 mx-auto p-5"
+      >
+        <Input name="product" placeholder="Enter product name..." />
+        <Input name="price" placeholder="Enter price name..." />
+        <Button>Add Product</Button>
+      </form>
+
+      <h2 className="font-bold p-5">List of Products</h2>
+      <div className="flex flex-wrap gap-5">
+        {products.map((product) => (
+          <div key={product.id} className="p-5 shadow">
+            <p className="font-semibold">{product.product}</p>
+            <p className="text-gray-600">$ {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,19 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Product } from "@/types/product";
-import { addProductToDatabase } from "@/actions/serverAction";
-import AddProductButton from "@/components/AddProductButton";
 
-export default async function Home() {
-  const res = await fetch(
-    "https://653918f7e3b530c8d9e7e7b1.mockapi.io/products",
-    {
-      cache: "no-cache",
-      next: {
-        tags: ["produts"],
-      },
-    }
-  );
-
-  const products: Product[] = await res.json();
-
+export default function Home() {
   return (
-    <main className="my-6 max-w-4xl mx-auto">
-      <h1 className="text-center text-3xl font-bold">Products Warehouse</h1>
-
-      <AddProductButton>Add Macbook Pro</AddProductButton>
-
-      <form
-        action={addProductToDatabase}
-        className="max-w-lg flex flex-col gap-5 mx-auto p-5"
-      >
-        <Input name="product" placeholder="Enter product name..." />
-        <Input name="price" placeholder="Enter price name..." />
-        <Button>Add Product</Button>
-      </form>
-
-      <h2 className="font-bold p-5">List of Products</h2>
-      <div className="flex flex-wrap gap-5">
-        {products.map((product) => (
-          <div key={product.id} className="p-5 shadow">
-            <p className="font-semibold">{product.product}</p>
-            <p className="text-gray-600">$ {product.price}</p>
-          </div>
-        ))}
+    <div className="grid min-h-screen place-items-center">
+      <div className="flex gap-5">
+        <Link href="/example-1" className={cn(buttonVariants())}>
+          Example 1
+        </Link>
+        <Link href="/example-2" className={cn(buttonVariants())}>
+          Example 2
+        </Link>
       </div>
-    </main>
+    </div>
   );
 }
